c6492098360d290479deb9b4e8f27adc9a626efd
Spenser Black <spenserblack01@gmail.com>
2021-12-04 16:57:02 -0500

    Test `SetL`

--- byteutils_test.go
+++ byteutils_test.go
@@ -4,5 +4,9 @@ import "testing"
 
 // TestSetL checks that a bit from the left will be set.
 func TestSetL(t *testing.T) {
-	
+	b := Byte(0)
+	b.SetL(0)
+	if b != 0b1000_0000 {
+		t.Fatalf(`b = %08b, want 10000000`, b)
+	}
 }
