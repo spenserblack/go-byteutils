a4e00d790e7c929335e6c59b7742dd798370df88
Spenser Black <spenserblack01@gmail.com>
2021-12-04 18:54:09 -0500

    Implement byte utilities

--- byteutils.go
+++ byteutils.go
@@ -1,5 +1,105 @@
+// Utilities for managing bytes.
+//
+// Note that a byte's indices are [0, 8), and using an index beyond this range
+// may cause a panic or undefined behavior.
 package byteutils
 
-func Add(l, r int) int {
-	return l + r
+// Byte is a wrapper around the built-in byte that provides additonal
+// functionality.
+type Byte byte
+
+// Bit abstracts a single bit. The underlying type is byte instead of
+// bool to make conversion usage with bytes simpler.
+type Bit byte
+
+const (
+	// Zero represents a 0 bit
+	Zero Bit = 0
+	// One represents a 1 bit
+	One Bit = 1
+)
+
+// SetL sets the nth bit from the left.
+func (b *Byte) SetL(index byte) {
+	b.SetR(7 - index)
+}
+
+// ClearL clears the nth bit from the left.
+func (b *Byte) ClearL(index byte) {
+	b.ClearR(7 - index)
+}
+
+// SetR sets the nth bit from the right.
+func (b *Byte) SetR(index byte) {
+	*b |= (1 << index)
+}
+
+// ClearR clears the nth bit from the right.
+func (b *Byte) ClearR(index byte) {
+	*b &= ^(1 << index)
+}
+
+// ChangeL changes the nth bit from the right to the bit provided.
+func (b *Byte) ChangeL(index byte, bit Bit) {
+	b.ChangeR(7 - index, bit)
+}
+
+// ChangeR changes the nth bit from the left to the bit provided.
+func (b *Byte) ChangeR(index byte, bit Bit) {
+	if bit.AsBool() {
+		b.SetR(index)
+	} else {
+		b.ClearR(index)
+	}
+}
+
+// GetL gets the nth bit from the right.
+func (b Byte) GetL(index byte) Bit {
+	return b.GetR(7 - index)
+}
+
+// GetR gets the nth bit from the left.
+func (b Byte) GetR(index byte) Bit {
+	return Bit(b & (1 << index)).normalize()
+}
+
+// ToggleL flips the nth bit from the left.
+func (b *Byte) ToggleL(index byte) {
+	b.ToggleR(7 - index)
+}
+
+// ToggleR flips the nth bit from the right.
+func (b *Byte) ToggleR(index byte) {
+	*b ^= 1 << index
+	
+}
+
+// NewBit creates a bit. This is to simplify enforcing that a bit is either 
+// Zero or One. Boolean type and most numerical types can be used as the bit.
+// Floats, complex types, etc. are undefined behavior.
+func NewBit(bit interface{}) Bit {
+	set := false
+	switch v := bit.(type) {
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		set = v != 0
+	case bool:
+		set = v
+	}
+	if set {
+		return One
+	}
+	return Zero
+}
+
+// AsBool convers a bit into a bool.
+func (b Bit) AsBool() bool {
+	return b != Zero
+}
+
+// Normalize forces a bit to be either Zero or One
+func (b Bit) normalize() Bit {
+	if b.AsBool() {
+		return One
+	}
+	return Zero
 }
--- byteutils_test.go
+++ byteutils_test.go
@@ -2,8 +2,93 @@ package byteutils
 
 import "testing"
 
-func SanityCheck(t *testing.T) {
-	if Add(1, 2) != 3 {
-		t.Fatalf("1 + 2 != 3")
+// TestAsBool checks that a bit is false when it is 0, true otherwise.
+func TestAsBool(t *testing.T) {
+	if Zero.AsBool() {
+		t.Error("want Zero to be false")
+	}
+	if !One.AsBool() {
+		t.Error("want One to be true")
+	}
+}
+
+// TestNewBit checks that NewBit returns One for non-zero values with defined
+// behavior, Zero otherwise.
+func TestNewBit(t *testing.T) {
+	if NewBit(true) != One {
+		t.Error("want true bit to be set")
+	}
+	if NewBit(0) != Zero {
+		t.Error("want zero bit to be clear")
+	}
+	if NewBit("undefined") != Zero {
+		t.Error("expected undefined behavior to leave bit clear")
+	}
+}
+
+// TestSetL checks that a bit from the left will be set.
+func TestSetL(t *testing.T) {
+	b := Byte(0)
+	b.SetL(0)
+	if b != 0b1000_0000 {
+		t.Fatalf(`b = %08b, want 10000000`, b)
+	}
+}
+
+// TestSetR checks that a bit from the left will be set.
+func TestSetR(t *testing.T) {
+	b := Byte(0)
+	b.SetR(0)
+	if b != 0b0000_0001 {
+		t.Fatalf(`b = %08b, want 00000001`, b)
+	}
+}
+
+// TestClear checks that a bits are cleared.
+func TestClear(t *testing.T) {
+	b := Byte(0xFF)
+	b.ClearL(1)
+	b.ClearR(0)
+	want := Byte(0b1011_1110)
+	if b != want {
+		t.Fatalf(`b = %08b, want %08b`, b, want)
+	}
+}
+
+// TestChange checks that bits are changed.
+func TestChange(t *testing.T) {
+	b := Byte(0x0F)
+
+	b.ChangeL(0, One)
+	b.ChangeR(0, Zero)
+	want := Byte(0b1000_1110)
+	if b != want {
+		t.Fatalf(`b = %08b, want %08b`, b, want)
+	}
+}
+
+// TestGet checks that the correct value is returned when "indexing" into
+// a byte.
+func TestGet(t *testing.T) {
+	b := Byte(0x0F)
+
+	if actual := b.GetL(4); actual != One {
+		t.Errorf(`4 from left of %08b = %v, want %v`, b, actual, One)
+	}
+	if actual := b.GetR(4); actual != Zero {
+		t.Errorf(`4 from right of %08b = %v, want %v`, b, actual, Zero)
+	}
+}
+
+// TestToggle checks that a bit is flipped and remaining bits remain the same.
+func TestToggle(t *testing.T) {
+	b := Byte(0x0F)
+	b.ToggleR(3)
+	if b != 0x07 {
+		t.Errorf(`b = %08b, want %08b`, b, 0x07)
+	}
+	b.ToggleL(3)
+	if b != 0x17 {
+		t.Errorf(`b = %08b, want %08b`, b, 0x17)
 	}
 }
