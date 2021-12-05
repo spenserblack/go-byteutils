6c537f3d32dc3175e48596e5f7a55a95f36c0663
Spenser Black <spenserblack01@gmail.com>
2021-12-05 11:36:19 -0500

    Fix actual problem

--- conversion_test.go
+++ conversion_test.go
@@ -33,20 +33,14 @@ func TestIteration(t *testing.T) {
 	bigEndianOrder := []Byte{0x00, 0x08, 0x10}
 	littleEndianOrder := []Byte{0x10, 0x08, 0x00}
 
-	LittleEndian.IterateSmallestToLargest(
-		b Byte, 
-		func(b Byte, index int) {
-			if want := littleEndianOrder[index]; b != want {
-				t.Errorf(`little endian b = %02X, want %02X`, b, want)
-			}
-		},
-	)
-	BigEndian.IterateSmallestToLargest(
-		b Byte, 
-		func(b Byte, index int) {
-			if want := bigEndianOrder[index]; b != want {
-				t.Errorf(`big endian b = %02X, want %02X`, b, want)
-			}
-		},
-	)
+	LittleEndian.IterateSmallestToLargest(b, func(b Byte, index int) {
+		if want := littleEndianOrder[index]; b != want {
+			t.Errorf(`little endian b = %02X, want %02X`, b, want)
+		}
+	})
+	BigEndian.IterateSmallestToLargest(b, func(b Byte, index int) {
+		if want := bigEndianOrder[index]; b != want {
+			t.Errorf(`big endian b = %02X, want %02X`, b, want)
+		}
+	})
 }
