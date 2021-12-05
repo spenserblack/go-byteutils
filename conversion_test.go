40ca9c6200f8a19b633b5f69b29ebfae6c9f4976
Spenser Black <spenserblack01@gmail.com>
2021-12-04 22:15:49 -0500

    Pad hex values

--- conversion_test.go
+++ conversion_test.go
@@ -7,10 +7,10 @@ func TestToUint16(t *testing.T) {
 	var b Convert = Bytes{0x0F, 0xA0}
 
 	if actual := b.ToUint16(BigEndian); actual != 0xA00F {
-		t.Errorf(`result = %X, want A00F`, actual)
+		t.Errorf(`result = %04X, want A00F`, actual)
 	}
 	if actual := b.ToUint16(LittleEndian); actual != 0x0FA0 {
-		t.Errorf(`result = %X, want 0FA0`, actual)
+		t.Errorf(`result = %04X, want 0FA0`, actual)
 	}
 }
 
@@ -19,9 +19,9 @@ func TestToUint32(t *testing.T) {
 	var b Convert = Bytes{0x0F, 0x00, 0x00, 0xA0}
 
 	if actual := b.ToUint32(BigEndian); actual != 0xA0_00_00_0F {
-		t.Errorf(`result = %X, want A000000F`, actual)
+		t.Errorf(`result = %08X, want A000000F`, actual)
 	}
 	if actual := b.ToUint32(LittleEndian); actual != 0x0F_00_00_A0 {
-		t.Errorf(`result = %X, want 0F0000A0`, actual)
+		t.Errorf(`result = %08X, want 0F0000A0`, actual)
 	}
 }
