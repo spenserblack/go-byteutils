d82503218dde6464a9da9e0ddfa2d6b9c810c831
Spenser Black <spenserblack01@gmail.com>
2021-12-05 11:31:44 -0500

    Fix iteration test

--- conversion_test.go
+++ conversion_test.go
@@ -33,17 +33,14 @@ func TestIteration(t *testing.T) {
 	bigEndianOrder := []Byte{0x00, 0x08, 0x10}
 	littleEndianOrder := []Byte{0x10, 0x08, 0x00}
 
-	LittleEndian.IterateSmallestToLargest(b, TestIterationHelper(t, littleEndianOrder))
-	BigEndian.IterateSmallestToLargest(b, TestIterationHelper(t, bigEndianOrder))
-}
-
-// TestIterationHelper is a helper for checking proper iteration. It returns a
-// function for iteration.
-func TestIterationHelper(t *testing.T, ordered []Byte) ByteIteratorFunc {
-	t.Helper()
-	return func(b Byte, index int) {
-		if want := ordered[index]; b != want {
-			t.Errorf(`b = %02X, want %02X`, b, want)
+	LittleEndian.IterateSmallestToLargest(b Byte, func(b Byte, index int) {
+		if want := littleEndianOrder[index]; b != want {
+			t.Errorf(`little endian b = %02X, want %02X`, b, want)
 		}
-	}
+	})
+	BigEndian.IterateSmallestToLargest(b Byte, func(b Byte, index int) {
+		if want := bigEndianOrder[index]; b != want {
+			t.Errorf(`big endian b = %02X, want %02X`, b, want)
+		}
+	})
 }
