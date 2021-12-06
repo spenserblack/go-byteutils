ad54716881b45eb7348272b11dc353f5d28357be
Spenser Black <spenserblack01@gmail.com>
2021-12-05 19:29:42 -0500

    Fix bit shift

--- endianness.go
+++ endianness.go
@@ -45,7 +45,7 @@ func (e Endian) IterateUint16(n uint16, f ByteIteratorFunc) {
 	} else {
 		for i := smallest; i >= largest; i-- {
 			enumeration := smallest - i
-			shift := (largest - i) * 8
+			shift := i * 8
 			var intersect uint16 = 0xFF << shift
 			b := Byte((n & intersect) >> shift)
 			f(b, enumeration)
